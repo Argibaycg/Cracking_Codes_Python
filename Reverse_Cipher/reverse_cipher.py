9a514dccf84f58a96211c547068809a003cd9657
Argibay Gaston <argibaycg@gmail.com>
2020-11-08 15:08:05 -0300

    Add input to the Reverse Cipher example

--- Reverse_Cipher/reverse_cipher.py
+++ Reverse_Cipher/reverse_cipher.py
@@ -1,7 +1,7 @@
 # Reverse Cipher
 # https://www.nostarch.com/crackingcodes/ (BSD Licenced)
 
-message = 'This is the message that you want to cipher'
+message = input('Shh, what do you want to hide?:')
 translated = ''
 
 i = len(message) - 1
@@ -10,4 +10,4 @@ while i >= 0:
     translated = translated + message[i]
     i = i - 1
 
-print(translated)
+print('It\'s secret now: ', translated)
